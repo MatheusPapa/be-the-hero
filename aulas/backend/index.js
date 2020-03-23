3d897d39c7f53f831f80a0fcba44005e4c751ccf
Matheus <matheusalmeida13.ma@gmail.com>
2020-03-23 11:12:16 -0400

    Criando frontend com React

---
+++ aulas/backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+
+  return response.json({ 
+    evento: "Semana OmniStack 11.0",
+    aluno: "Matheus Papa de Almeida"
+  });
+
+});
+
+app.listen(3333);
