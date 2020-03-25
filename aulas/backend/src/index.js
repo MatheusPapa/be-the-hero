58cd9842e12490808aba0e448bd79af09aebefc2
Matheus <matheusalmeida13.ma@gmail.com>
2020-03-25 12:53:52 -0400

    Conclusão do back-end da aplicação

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+
+const app = express();
+
+app.cors(cors());
+app.use(express.json());
+app.use(routes);
+/*
+  Rota / Recurso
+*/
+/*
+  Métodos HTTP:
+    
+  GET: Buscar uma informação do back-end
+  POST: Criar uma informação no back-end
+  PUT: Alterar uma informação no back-end
+  DELETE: Deletar uma informação no back-end
+*/
+
+/*
+  Tipos de parâmetros
+
+  Query Params: parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+  Route Params: parâmetros utilizados para identificar recursos
+  Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/*
+  SQL: MySQL, SQLite, PostgreeSQL, Oracle, Microsoft SQL Server
+  NoSQL: MongoDB, CouchDB, etc
+*/
+
+/*
+  Driver: SELECT * FROM users
+  Query Builder: table('users').select('*').where() => KNEX.JS
+*/
+
+
+
+app.listen(3333);
